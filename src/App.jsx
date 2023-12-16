ca898537470a52d3573f450abfc5df37648facad
Abraham Cabrera <ingeabraham23@gmail.com>
2023-12-16 12:42:02 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import monjita from './assets/monjita.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='contenedor'>
+      <h1>Rifa con causa</h1>
+      <img src= {monjita} />
+    </div>
+  )
+}
+
+export default App
